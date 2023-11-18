4b6e0b728e44ab635d9bb6b8e70af94c612b980d
vishant <vishant.96.sehrawat@gmail.com>
2023-11-18 22:15:34 +0530

    added the auth middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,49 +1,50 @@
-// For only allowing logged in users to access things on the website
-
-var jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { UserModel } = require("../models/user.model");
 const { BlacklistModel } = require("../models/blacklist.model");
-const { redisClient } = require("../configs/redis");
-
 require("dotenv").config();
 
 const authMiddleware = async (req, res, next) => {
-  console.log(
-    "🚀 ~ file: authMiddleware.middleware.js:9 ~ authMiddleware ~ req:",
-    req.body
-  );
-  const token = await redisClient.get("jwttoken"); // we have saved token on redis
-  // const token = req.headers.authorization;
-  console.log(
-    "🚀 ~ file: au   thentication.js:8 ~ authMiddleware ~ token:",
-    token
-  );
   try {
-    // checking if the token is present in blacklist or not
-    const blackToken = await BlacklistModel.find({ token: token });
-    // console.log("🚀 ~ file: authentication.js:12 ~ authMiddleware ~ blackToken:", blackToken)
-    if (blackToken.length >= 1) {
-      res.status(401).send({ msg: "User blacklisted  login again" });
-    } else {
-      // decode the token
-      var decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-      // console.log("🚀 ~ file: authentication.js:18 ~ authMiddleware ~ decoded:", decoded)
-      if (decoded) {
-        req.body.userId = decoded.userId;
-        const user = await UserModel.findOne({ _id: decoded.userId });
-        // console.log("🚀 ~ file: authentication.js:23 ~ authMiddleware ~ user:", user)
-        req.role = user.role;
-        next();
-      } else {
-        res.status(401).send({ msg: "authorization error" });
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({
+        message: "Authorization token missing",
+        success: false,
+      });
+    }
+
+    const blackToken = await BlacklistModel.findOne({ token });
+    if (blackToken) {
+      return res.status(401).json({
+        message: "User blacklisted, please login again",
+        success: false,
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    if (decoded) {
+      req.body.userId = decoded.userId;
+      const user = await UserModel.findOne({ _id: decoded.userId });
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+          success: false,
+        });
       }
+      req.role = user.role; //^? we can also send other details of the user with the request
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Authorization error",
+        success: false,
+      });
     }
   } catch (error) {
-    console.log(
-      "🚀 ~ file: authMiddleware.middleware.js:34 ~ authMiddleware ~ error:",
-      error
-    );
-    res.send(error.message);
+    console.error("Authentication error:", error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
   }
 };
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,6 +6,7 @@ const dentalUserSchema = mongoose.Schema(
     name: { type: String, required: true },
     email: { type: mongoose.SchemaTypes.Email, required: true, unique: true },
     password: { type: String, required: true },
+    username: { type: String, unique: true },
     dateOfBirth: { type: Date },
     gender: { type: String, enum: ["male", "female", "other"] },
     nationality: { type: String },
@@ -19,6 +20,32 @@ const dentalUserSchema = mongoose.Schema(
       country: { type: String },
       zipCode: { type: String },
     },
+    emergencyContacts: [
+      {
+        name: { type: String },
+        relationship: { type: String },
+        phoneNumber: { type: String },
+      },
+    ],
+
+    paymentInfo: {
+      paymentMethods: [
+        {
+          methodType: { type: String },
+          cardNumberLastFour: { type: String },
+          expirationDate: { type: String },
+        },
+      ],
+      billingAddress: {
+        fullName: { type: String },
+        streetAddress: { type: String },
+        city: { type: String },
+        state: { type: String },
+        postalCode: { type: String },
+        country: { type: String },
+        phoneNumber: { type: String },
+      },
+    },
 
     medicalHistory: {
       allergies: [{ type: String }],
@@ -26,15 +53,43 @@ const dentalUserSchema = mongoose.Schema(
       comments: { type: String },
     },
 
+    languagePreferences: [{ type: String }],
     notificationSettings: {
       email: { type: Boolean, default: true },
       sms: { type: Boolean, default: false },
       pushNotifications: { type: Boolean, default: true },
     },
+    preferences: {
+      preferredBarber: { type: mongoose.Schema.Types.ObjectId, ref: "barber" },
+      preferredService: { type: String },
+    },
+
+    loginHistory: [
+      {
+        timestamp: { type: Date },
+        ipAddress: { type: String },
+        deviceType: { type: String },
+        browser: { type: String },
+        location: {
+          latitude: { type: Number },
+          longitude: { type: Number },
+        },
+      },
+    ],
 
     lastLogin: {
       timestamp: { type: Date },
       ipAddress: { type: String },
+    },
+    socialMedia: {
+      facebook: {
+        type: String,
+        match: /^(https?:\/\/)?(www\.)?facebook.com\/.+$/,
+      },
+      twitter: {
+        type: String,
+        match: /^(https?:\/\/)?(www\.)?twitter.com\/[a-zA-Z0-9_]+$/,
+      },
     },
 
     role: { type: String, enum: ["user", "admin"], default: "user" }, // not much useful here but still can be used differently
@@ -43,6 +98,15 @@ const dentalUserSchema = mongoose.Schema(
     isActive: { type: Boolean, default: true },
     passwordResetToken: { type: String },
     otpEmail: {
+      code: { type: String },
+      expiration: { type: Date },
+      attempts: { type: Number, default: 0 },
+      maxAttempts: { type: Number, default: 3 },
+      lastAttempt: { type: Date },
+      used: { type: Boolean, default: false },
+    },
+
+    otpMobile: {
       code: { type: String },
       expiration: { type: Date },
       attempts: { type: Number, default: 0 },
@@ -65,6 +129,35 @@ const dentalUserSchema = mongoose.Schema(
       night: { type: Boolean, default: false },
       lateNight: { type: Boolean, default: false },
     },
+
+    averageUserRating: { type: Number, default: 0 },
+
+    subscription: {
+      loyaltyPoints: { type: Number, default: 0 },
+      details: {
+        planType: {
+          type: String,
+          enum: ["basic", "premium", "superpremium"],
+          default: "basic",
+        },
+        startDate: { type: Date },
+        endDate: { type: Date },
+        isActive: { type: Boolean, default: false },
+      },
+    },
+
+    specialRequests: [{ type: String }],
+
+    appointmentHistory: [
+      {
+        barberId: { type: mongoose.Schema.Types.ObjectId, ref: "barber" },
+        appointmentDate: { type: Date },
+        serviceBooked: { type: String },
+        feedback: { type: String },
+      },
+    ],
+
+    favoriteHairstyles: [{ type: String }],
 
     // Dental Booking Specific Fields
     dentalHistory: {
