77749b47fdf5730d83e00d238080fef05f8d5113
vishant <vishantsehrawat2@gmail.com>
2023-11-28 23:12:34 +0530

    added a few more routes for dentist

--- backend/controllers/dentist.controller.js
+++ backend/controllers/dentist.controller.js
@@ -86,6 +86,158 @@ const dentistLogin = async (req, res) => {
     });
   }
 };
+const getDentistById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dentist = await DentistModel.findById(id);
+    if (!dentist) {
+      return res.status(404).json({
+        message: "Dentist not found",
+        success: false,
+      });
+    }
+    return res.status(200).json({ dentist, success: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+const deleteDentistById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedDentist = await DentistModel.findByIdAndDelete(id);
+    if (!deletedDentist) {
+      return res.status(404).json({
+        message: "Dentist not found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      message: "Dentist deleted successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+const updateDentistPassword = async (req, res) => {
+  const { id } = req.params;
+  const { newPassword } = req.body;
+
+  try {
+    const dentist = await DentistModel.findById(id);
+    if (!dentist) {
+      return res.status(404).json({
+        message: "Dentist not found",
+        success: false,
+      });
+    }
+    const hash = bcrypt.hashSync(newPassword, 4);
+    dentist.password = hash;
+    await dentist.save();
+    return res.status(200).json({
+      message: "Dentist password updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+const getDentistAppointments = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dentist = await DentistModel.findById(id).populate(
+      "appointmentHistory"
+    );
+    if (!dentist) {
+      return res.status(404).json({
+        message: "Dentist not found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      appointments: dentist.appointmentHistory,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+const setDentistAvailability = async (req, res) => {
+  const { id } = req.params;
+  const { from, to } = req.body;
+
+  try {
+    const dentist = await DentistModel.findById(id);
+    if (!dentist) {
+      return res.status(404).json({
+        message: "Dentist not found",
+        success: false,
+      });
+    }
+    dentist.workingHours = { from, to };
+    await dentist.save();
+    return res.status(200).json({
+      message: "Dentist availability updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+const updateDentistDetails = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dentist = await DentistModel.findByIdAndUpdate(id, req.body);
+
+    if (!dentist) {
+      return res.status(404).json({
+        message: "Dentist not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Dentist details updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+
 
 module.exports = {
   registerDentist,
