975341ffa6600be266de4237562b31f7406981cb
vishant <vishantsehrawat2@gmail.com>
2023-11-28 23:51:27 +0530

    added a few more routes that will be used to work with website functionalities for fetching diff type of data

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -24,5 +24,22 @@ productRouter.patch("/:id", updateProduct);
 
 productRouter.delete("/:id", deleteProduct);
 
+productRouter.get("/", getAllProducts);
+
+productRouter.get("/category/:categoryName", getProductsByCategory);
+
+productRouter.get("/manufacturer/:manufacturerName", getProductsByManufacturer);
+
+productRouter.get("/search", searchProducts);
+
+productRouter.get("/featured", getFeaturedProducts);
+
+productRouter.get("/related/:id", getRelatedProducts);
+
+productRouter.post("/:id/reviews", addProductReview);
+
+productRouter.post("/:id/like", likeProduct);
+
+productRouter.post("/:id/share", shareProduct);
 
 module.exports = { productRouter };
