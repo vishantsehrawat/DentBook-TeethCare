ac19c4342d697a3c8609c10e7e5570cf4d62a7b3
vishant <vishantsehrawat2@gmail.com>
2023-12-06 14:04:19 +0530

    did initial setup for frontend in react

---
+++ dentbook-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>dental book appfdf</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ dentbook-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+// rafc shortcut for creating basic structure for the file
+import React from "react";
+import Footer from "../../components/layout/footer";
+
+
+const home = () => {
+  // all function and use states should be above the return
+  return <div>home
+
+
+    <Footer></Footer>
+  </div>;
+};
+
+export default home;
+    
