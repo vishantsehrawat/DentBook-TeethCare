062b9ca842121e505373511a1c271ccc437ac73e
vishant <vishantsehrawat2@gmail.com>
2023-12-08 16:37:57 +0530

    update appointment  route added

--- backend/controllers/appointment.controller.js
+++ backend/controllers/appointment.controller.js
@@ -65,6 +65,37 @@ const getAppointmentById = async (req, res) => {
   }
 };
 
+async function updateAppointment(req, res) {
+  try {
+    const { appointmentId } = req.params;
+    const updatedData = req.body;
+
+    const appointment = await AppointmentModel.findByIdAndUpdate(
+      appointmentId,
+      updatedData,
+      { new: true }
+    );
+
+    if (!appointment) {
+      return res
+        .status(404)
+        .json({ message: "Appointment not found", success: false });
+    }
+
+    return res.status(200).json({
+      message: "Appointment updated successfully",
+      success: true,
+      appointment,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Failed to update appointment",
+      success: false,
+      error: error.message,
+    });
+  }
+}
 module.exports = {
   createAppointment,
   getAllAppointments,
