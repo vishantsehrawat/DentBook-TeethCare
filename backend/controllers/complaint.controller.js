950074f60b136811a8e08ad93c31c16822ba6c40
vishant <vishantsehrawat2@gmail.com>
2023-12-10 18:17:32 +0530

    create and updat ecomplaint added

--- backend/controllers/complaint.controller.js
+++ backend/controllers/complaint.controller.js
@@ -43,7 +43,66 @@ const getComplaintById = async (req, res) => {
   }
 };
 
+const createComplaint = async (req, res) => {
+  try {
+    const newComplaint = new ComplaintModel(req.body);
+    const savedComplaint = await newComplaint.save();
+
+    return res.status(200).json({
+      message: "Complaint created successfully",
+      success: true,
+      complaint: savedComplaint,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Cannot create complaint",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const updateComplaint = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedComplaint = await ComplaintModel.findByIdAndUpdate(
+      id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedComplaint) {
+      return res.status(404).json({
+        message: "Complaint not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Complaint updated successfully",
+      success: true,
+      complaint: updatedComplaint,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Cannot update complaint",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
+  createComplaint,
+  updateComplaint,
+  deleteComplaint,
+  getComplaintsByUser,
+  getComplaintsByStatus,
+  resolveComplaint,
   getAllComplaints,
   getComplaintById,
 };
